b23b6ea95c6e30b07026192d9c7e956369f234d1
minhtrungs013 <trungs0132@gmail.com>
2023-07-24 09:18:47 +0700

    TTTE-4 #3 Create Practice User Home Page

--- qlbh/src/components/user/practice/practice.jsx
+++ qlbh/src/components/user/practice/practice.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import "./practice.css"
-import { Col, Row } from 'antd';
-import { Link  } from 'react-router-dom';
+import { Col, Row, Spin } from 'antd';
+import { Link } from 'react-router-dom';
 import { getAllPracticesWithoutParts } from '../../../api/service/paractice/paractice';
 
 export default function Practice({ onClickPracticeId }) {
 
     const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
 
     const setPracticeId = (practice) => {
         if (practice && practice.type) {
@@ -15,9 +16,11 @@ export default function Practice({ onClickPracticeId }) {
     }
 
     useEffect(() => {
+        setLoading(true)
         const getAllPracticesWithoutPart = () => {
             getAllPracticesWithoutParts(`practices`)
                 .then((res) => {
+                    setLoading(false)
                     setData(res.data.data);
                 }).catch((Error) => {
                     console.log(Error)
@@ -31,18 +34,26 @@ export default function Practice({ onClickPracticeId }) {
             <Row gutter={1} >
                 <Col span={16} offset={4} className=''>
                     <h1 className='practice__heading'>Practice </h1>
-                    <Row gutter={100} mt={10}>
-                        {data?.map((item) => (
-                            <Col span={6} key={item.id}>
-                                <Link to={`/practice/${item.type}`} onClick={() => setPracticeId(item)} className='practice__link'>
-                                    <div className='practice'>
-                                        <img className='practice_img' src={item.image} alt="" />
-                                        <h2>{item.name}</h2>
-                                    </div>
-                                </Link>
-                            </Col>
-                        ))}
-                    </Row>
+                    {loading ?
+                        <div className="example">
+                            <Spin />
+                        </div> :
+                        <>
+                            <Row gutter={100} mt={10}>
+                                {data?.map((item) => (
+                                    <Col span={6} key={item.id}>
+                                        <Link to={`/practice/${item.type}`} onClick={() => setPracticeId(item)} className='practice__link'>
+                                            <div className='practice'>
+                                                <img className='practice_img' src={item.image} alt="" />
+                                                <h2>{item.name}</h2>
+                                            </div>
+                                        </Link>
+                                    </Col>
+                                ))}
+                            </Row>
+
+                        </>
+                    }
                 </Col>
             </Row>
         </div>
