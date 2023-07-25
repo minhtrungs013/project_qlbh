e720049cb905321a9bd484a8c7715b5aeead1608
minhtrungs013 <trungs0132@gmail.com>
2023-07-25 09:53:01 +0700

    TTTE-5 #1 Create Listening Practice

--- qlbh/src/components/redux/_actions/user.actions.js
+++ qlbh/src/components/redux/_actions/user.actions.js
@@ -1,7 +1,6 @@
 import { Constants } from "../_constants/constants";
 
 export const setPracticeId = (id) => {
-    console.log(id);
     return {
       type: Constants.SET_PRACTICE_ID,
       payload: id,
